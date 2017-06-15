1812e98eb0d3dbe6962d81ebf1b7e4e2a015f801
Jeb Runnoe <jebrunnoe@gmail.com>
2017-06-15 10:43:50 -0700

    Problem 10 revised and commented

--- problem10.py
+++ problem10.py
@@ -1,11 +1,27 @@
-import math
+#PROBLEM:
+#  10
+#
+#NAME:
+#  "Summation of Primes"
+#
+#PROMPT:
+#  The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17. 
+#  Find the sum of all the primes below two million.
+#
+#LINK:
+#  https://projecteuler.net/problem=10
 
-def prime(n):
-    for i in range(2, int(math.sqrt(n) + 1)):
-	if n % i == 0: return False
-    return True
-accum = 2
-for i in range(3, 2*pow(10, 6), 2):
-    if prime(i):
-	accum += i
-print accum
+from math import sqrt
+
+def is_prime(n):
+   for i in range(2, int(sqrt(n) + 1)):
+      if n % i == 0: 
+	 return False
+   return True
+
+sum = 2	 # Set sum equal to 2 to account for 2 being prime
+# Increment up to the limit by 2 to skip even numbers
+for i in range(3, 2000000, 2):
+   if is_prime(i):
+      sum += i
+print sum
