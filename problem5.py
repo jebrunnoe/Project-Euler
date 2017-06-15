37513b0fe2a48a888b6863d1dcd14caa8bffced6
Jeb Runnoe <jebrunnoe@gmail.com>
2017-06-15 08:59:29 -0700

    Problem 5 revised and commented

--- problem5.py
+++ problem5.py
@@ -1,12 +1,18 @@
-n = 1067602
-r = str(n)[::2]
-print r
-#palindrome = 0
-#for i in range(1, 1000):
-#   for j in range(1, 1000):
-#	product = str(i * j)
-#	reverse = str(product)[::-1]
-#	if product == reverse: 
-#	    if int(product) > palindrome:
-#		palindrome = int(product)
-#print palindrome
+#PROBLEM:
+#  5
+#
+#NAME: 
+#  "Smallest Multiple"
+#
+#PROMPT:
+#  2520 is the smallest number that can be divided by each 
+#  of the numbers from 1 to 10 without any remainder. What 
+#  is the smallest positive number that is evenly divisible 
+#  by all of the numbers from 1 to 20?
+
+# For this problem, all that is to break down each number 
+# 1-20 into it's prime factorization, noting the power on 
+# prime. Then the product of each prime raised to the highest
+# power that appears will produce the answer.
+
+print 1 * 2**4 * 3**2 * 5 * 7 * 11 * 13 * 17 * 19
