37723046d1199524d987be8c1cea43e09b0b3c4f
Jeb Runnoe <jebrunnoe@gmail.com>
2017-06-14 20:16:23 -0700

    Problem 3 revised and commented

--- problem3.py
+++ problem3.py
@@ -1,9 +1,26 @@
-n = 600851475143
-f = 2
-factors = []
-while n >= f:
-    while n % f == 0:
-	factors.append(f)
-	n = n / f
-    f += 1
-print max(factors)
+#PROBLEM: 
+#  3
+#
+#NAME: 
+#  "Largest Prime Factor"
+#
+#PROMPT:
+#  The prime factors of 13195 are 5, 7, 13 and 29. 
+#  What is the largest prime factor of the number 600851475143 ?
+#
+#LINK:
+#  https://projecteuler.net/problem=3
+
+num = 600851475143
+
+def get_prime_factors(n):
+   factor = 2
+   prime_factors = []
+   while n >= factor:
+      while n % factor == 0:	
+	 prime_factors.append(factor) #'factor' is a prime factor of n, save it
+	 n = n / factor   # repeatedly divide n by factor until n is no longer divisible by it
+      factor += 1
+   return prime_factors
+
+print max(get_prime_factors(num)) 
