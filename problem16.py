ab602b7a365625231db67f52cd4acc182b6791de
Jeb Runnoe <jebrunnoe@gmail.com>
2018-03-05 17:48:39 -0800

    Problem 16 revised and commented.

--- problem16.py
+++ problem16.py
@@ -1,5 +1,23 @@
-n = str(2**1000)
-s = 0
-for i in range(len(n)):
-    s+= int(n[i])
-print s
+#PROBLEM:
+#  16
+#
+#NAME:
+#  "Power Digit Sum"
+#
+#PROMPT:
+#  2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#  What is the sum of the digits of the number 2^1000?
+#
+#LINK:
+#  https://projecteuler.net/problem=16
+
+import time
+start_time = time.time()
+
+number = str(pow(2, 1000))
+digit_sum = 0
+for i in range(len(number)):
+    digit_sum+= int(number[i])
+
+print digit_sum
+print "Runtime: %.5fs" % (time.time() - start_time)
