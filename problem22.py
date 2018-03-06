6c87680879335e6fe0054127f8395bde115fd780
Jeb Runnoe <jebrunnoe@gmail.com>
2018-03-05 19:16:57 -0800

    Problem 22 revised and commented.

--- problem22.py
+++ problem22.py
@@ -1,15 +1,28 @@
+#PROBLEM:
+#  22
+#
+#NAME:
+#  "Name Scores"
+#
+#LINK:
+#  https://projecteuler.net/problem=22
+
+import time
+start_time = time.time()
+
 raw = open("p022_names.txt", "r").read()
-names = raw.replace('"', '').split(',')
+names = raw.replace('"', '').split(',') # Get rid of spaces, and parse the names into a list.
 names.sort()
-score = 0
+name_scores = list()
 
 def value(name):
     value = 0
     for i in range(len(name)):
-	value += ord(name[i]) % 32
+	value += ord(name[i]) % 32 # Alphabetical value.
     return value
 
 for i in range(len(names)):
-    score += value(names[i]) * (i + 1)
+    name_scores.append(value(names[i]) * (i + 1))
 
-print score
+print sum(name_scores)
+print "Runtime: %.5fs " % (time.time() - start_time)
