bd8565756985bcab1804b048756e3d9ca434f038
Jeb Runnoe <jebrunnoe@gmail.com>
2018-03-05 18:55:11 -0800

    Problem 20 revised and commented.

--- problem20.py
+++ problem20.py
@@ -1,6 +1,20 @@
+#PROBLEM:
+#  20
+#
+#NAME:
+#  "Factorial Digit Sum"
+#
+#LINK:
+#  https://projecteuler.net/problem=20
+
 import math
-n = str(math.factorial(100))
-f = 0
-for i in range(len(n)):
-    f += int(n[i])
-print f
+import time
+start_time = time.time()
+
+number = str(math.factorial(100))
+digit_sum = 0
+for i in range(len(number)):
+    digit_sum += int(number[i])
+
+print digit_sum
+print "Runtime: %.5fs " % (time.time() - start_time)
