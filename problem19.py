35894a6271099df8632f27b63722199c7ab2f249
Jeb Runnoe <jebrunnoe@gmail.com>
2018-03-05 18:48:31 -0800

    Problem 19 revised and commented.

--- problem19.py
+++ problem19.py
@@ -1,7 +1,23 @@
+#PROBLEM:
+#  19
+#
+#NAME:
+#  "Counting Sundays"
+#
+#LINK:
+#  https://projecteuler.net/problem=19
+
 import datetime
+import time
+start_time = time.time()
+
 count = 0
 day = datetime.date(1901, 1, 1)
+
 while day != datetime.date(2000, 12, 31):
-    if day.weekday() == 6 and day.day == 1: count += 1 
-    day = day + datetime.timedelta(1)
+   if day.weekday() == 6 and day.day == 1: 
+      count += 1 
+   day = day + datetime.timedelta(1)
+
 print count
+print "Runtime: %.5fs " % (time.time() - start_time)
