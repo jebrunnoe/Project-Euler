5d2461718ec5cd0d99838a4ab9cef00d2e8c5e10
Jeb Runnoe <jebrunnoe@gmail.com>
2018-03-05 19:10:23 -0800

    Problem 21 revised and commented.

--- problem21.py
+++ problem21.py
@@ -1,17 +1,35 @@
-def factor(n):
-    factors = [1]
-    for i in range(2, int(n ** 0.5) + 1):
-	if n % i == 0:
-	    factors.append(i)
-	    if n / i != i: factors.append(n / i)
-    return factors
+#PROBLEM:
+#  21
+#
+#NAME:
+#  Amicable Numbers
+#
+#LINK:
+#  https://projecteuler.net/problem=21
 
-a = []
-d = [0, 1]
+import time
+start_time = time.time()
+
+def get_divisors(n):
+   divisors = [1]
+   for i in range(2, int(n ** 0.5) + 1):
+      if n % i == 0:
+	 divisors.append(i)
+	 if n / i != i: 
+	    divisors.append(n / i)
+   return divisors # Return a list of the divisors of n.
+
+# Populate a lookup table of the sum of the divisors of n. 
+divisor_sums = [0, 1]
 for n in range(2, 10001): 
-    d.append(sum(factor(n)))
-	
+    divisor_sums.append(sum(get_divisors(n)))
+
+amicables = []
 for n in range(10001):
-   if d[n] < 10000 and d[n] != n and d[d[n]] == n: a.append(n)
+   m = divisor_sums[n]
+   # Check if both are within the limit, that they are not equal, and that they are amicable.
+   if m < 10000 and m != n and divisor_sums[m] == n: 
+      amicables.append(n)
     
-print sum(a)
+print sum(amicables)
+print "Runtime: %.5fs " % (time.time() - start_time)
