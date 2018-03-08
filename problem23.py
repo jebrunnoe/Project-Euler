22c0c4559a873e3669bf52866ae70b60d3acaaf1
Jeb Runnoe <jebrunnoe@gmail.com>
2018-03-07 17:49:46 -0800

    Problem 23 revised and commented.

--- problem23.py
+++ problem23.py
@@ -1,17 +1,36 @@
+#PROBLEM:
+#  23
+#
+#NAME:
+#  "Non-abundant Sums"
+#
+#LINK:
+#  https://projecteuler.net/problem=23
+#
+
+import time
+start_time = time.time()
+
 def factor(n):
-    factors = []
-    factors.append(1)
-    for i in range(2, int(n ** 0.5) + 1):
-	if n % i == 0:
-	    factors.append(i)
-	    if n / i != i: factors.append(n / i)
-    return factors
+   factors = list() 
+   factors.append(1)
+   for i in range(2, int(n ** 0.5) + 1):
+      if n % i == 0:
+	 factors.append(i)
+	 if n / i != i: 
+	    factors.append(n / i)
+   return factors
 
-abn = set()
-non = 0
+abundant_numbers = set()
+non_abundant_sum = list()
 
-for i in range(1, 20162): 
-    if sum(factor(i)) > i: abn.add(i)
-    if not any((i - a in abn) for a in abn): non += i
+for i in range(1, 28124): # Upper limit provided in prompt.
+   if sum(factor(i)) > i: # If true, i is abundant.
+      abundant_numbers.add(i)
+   # Suppose 'a' is an abundant number. If 'i - a' is also abundant, than
+   # i is the sum of the two abundant numbers 'a' and 'i - a'. 
+   if not any((i - a in abundant_numbers) for a in abundant_numbers):
+      non_abundant_sum.append(i)
 
-print non
+print sum(non_abundant_sum)
+print "Runtime: %.5fs " % (time.time() - start_time)
